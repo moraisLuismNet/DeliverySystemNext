10952d0a8bee0509a1c4f8cc08393bb3f3ff1e90
moraisLuismNet <morais.luism.net@gmail.com>
2026-08-27 15:23:59 +0200

    27.08.2026_restore auth session on reload and remove unused CartSync

--- src/components/common/BootstrapClient.tsx
+++ src/components/common/BootstrapClient.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useEffect } from "react";
+import { useAuthStore } from "@/store/authStore";
 export default function BootstrapClient() {
   useEffect(() => { require("bootstrap/dist/js/bootstrap.bundle.min.js"); }, []);
+  useEffect(() => { useAuthStore.getState().loadFromStorage(); }, []);
   return null;
 }
